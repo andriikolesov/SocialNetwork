a848a11716231d0719f9bf0b36613ba9970a2d6b
igor <joyukr>
2017-12-06 21:47:16 +0200

    added new entity group

---
+++ src/main/java/networks/model/Group.java
@@ -0,0 +1,18 @@
+package networks.model;
+
+/**
+ * @author Igor Hnes on 06.12.17.
+ */
+public class Group {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
