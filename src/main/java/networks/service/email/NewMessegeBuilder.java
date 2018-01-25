15ab3dbc6983138dcc6c1b03eab1bbf0b034fa83
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:50:13 +0000

    Update NewMessegeBuilder.java

--- src/main/java/networks/service/email/NewMessegeBuilder.java
+++ src/main/java/networks/service/email/NewMessegeBuilder.java
@@ -3,9 +3,6 @@ package networks.service.email;
 import networks.model.User;
 import networks.service.email.MessegeBuilder;
 
-/**
- * @author Igor Hnes on 20.12.17.
- */
 public class NewMessegeBuilder implements MessegeBuilder {
     @Override
     public String build(User user) {
