21c4cd411a68a3373e191de2afbd59bbf116961c
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 08:50:00 +0000

    Update KPI.java

--- src/main/java/tranning/Task6/KPI.java
+++ src/main/java/tranning/Task6/KPI.java
@@ -1,10 +1,6 @@
 package tranning.Task6;
 
-/**
- * @author Igor Hnes on 22.12.17.
- */
-
- class KPI extends Institute {
+class KPI extends Institute {
     @Override
     void study() {
         System.out.println("kpi");
