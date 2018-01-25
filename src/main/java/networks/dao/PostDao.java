9242aabb5f35d458d068baa37cdb1cd7e46bff83
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:30:11 +0000

    Update PostDao.java

--- src/main/java/networks/dao/PostDao.java
+++ src/main/java/networks/dao/PostDao.java
@@ -9,13 +9,8 @@ import networks.model.User;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * @author Igor Hnes on 06.12.17.
- */
 public interface PostDao {
 
-
     List<Post> getAll();
-
     List<Post> getByTitle(String title);
 }
