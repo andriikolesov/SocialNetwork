ec391c5139b109540ef72ccbc5abd386aa93c7f0
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:36:23 +0000

    Update UserService.java

--- src/main/java/networks/service/UserService.java
+++ src/main/java/networks/service/UserService.java
@@ -1,22 +1,14 @@
 package networks.service;
 
 import networks.model.User;
-
 import java.util.List;
 
-/**
- * @author Igor Hnes on 06.12.17.
- */
 public interface UserService extends CrudService <User, Long>{
 
     List<User> getByName(String name);
-
     List<User> getByAge(int age);
 
     void changePassword(User user, String newPassword);
-
     void happyBirthday(User user);
 
     }
-
-
