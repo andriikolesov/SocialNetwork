e5a29d5aa25a96f392a03cee70345647ec3ab17e
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:30:40 +0000

    Update UserDao.java

--- src/main/java/networks/dao/UserDao.java
+++ src/main/java/networks/dao/UserDao.java
@@ -1,15 +1,10 @@
 package networks.dao;
 
 import networks.model.User;
-
 import java.util.List;
 
-/**
- * @author Igor Hnes on 06.12.17.
- */
 public interface UserDao extends CrudDao<User, Long>{
 
     List<User> getByName(String name);
-
     List<User> getByAge(int age);
 }
