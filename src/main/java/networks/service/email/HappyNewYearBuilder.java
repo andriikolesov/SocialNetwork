4763e8a045b926aebd5a8630840c0a8dd749aca9
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:49:11 +0000

    Update HappyNewYearBuilder.java

--- src/main/java/networks/service/email/HappyNewYearBuilder.java
+++ src/main/java/networks/service/email/HappyNewYearBuilder.java
@@ -2,9 +2,6 @@ package networks.service.email;
 
 import networks.model.User;
 
-/**
- * @author Igor Hnes on 20.12.17.
- */
 public class HappyNewYearBuilder implements MessegeBuilder {
     @Override
     public String build(User user) {
