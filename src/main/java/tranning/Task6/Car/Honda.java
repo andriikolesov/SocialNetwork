8be29f5052d3f97e9128158d445edf1a4162c22f
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 08:52:16 +0000

    Update Honda.java

--- src/main/java/tranning/Task6/Car/Honda.java
+++ src/main/java/tranning/Task6/Car/Honda.java
@@ -1,11 +1,8 @@
 package tranning.Task6.Car;
 
-/**
- * @author Igor Hnes on 22.12.17.
- */
 public class Honda extends Car {
     @Override
     void drive() {
-        System.out.println("h");
+        System.out.println("honda");
     }
 }
