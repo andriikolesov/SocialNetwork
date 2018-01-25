43d0e984938c290e53f454b74ef347ae7791074e
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:35:53 +0000

    Update PostService.java

--- src/main/java/networks/service/PostService.java
+++ src/main/java/networks/service/PostService.java
@@ -1,16 +1,11 @@
 package networks.service;
 
 import networks.model.Post;
-
 import java.util.List;
 
-/**
- * @author Igor Hnes on 06.12.17.
- */
 public interface PostService extends CrudService {
 
     List<Post> getAll();
-
     List<Post> getByTitle(String title);
 
 }
