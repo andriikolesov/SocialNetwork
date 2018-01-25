7426e9db63737ee49d4ea14d1f7f10be2a65f690
Andrii Kolesov <andrii.kolesov@gmail.com>
2018-01-25 09:52:08 +0000

    Update RegistrationBuilder.java

--- src/main/java/networks/service/email/RegistrationBuilder.java
+++ src/main/java/networks/service/email/RegistrationBuilder.java
@@ -2,9 +2,6 @@ package networks.service.email;
 
 import networks.model.User;
 
-/**
- * @author Igor Hnes on 20.12.17.
- */
 public class RegistrationBuilder implements MessegeBuilder {
     @Override
     public String build(User user) {
